3858d3cfb0bd3d5c076a5eaa382396b8a585a04f
Shane Banks <shane.banks02@gmail.com>
2022-04-13 22:16:04 -0500

    Completed functest.js work

--- js/functest.js
+++ js/functest.js
@@ -1,23 +1,46 @@
 "use strict";
-/* alert("Let's check to see if you can take advantage of our 2-for-1 special.")
-let premium = confirm("If you a premium member, click OK.")
-let productQty = confirm("Are you buying more than one Commodore 64 today? If so, click OK.")
-let offerDate = confirm("If the offer is still valid, click OK.")
-let offerGood = (premium && productQty && offerDate);
-// console.log(offerGood);
-alert("You qualify for this offer if true: " + offerGood); */
 
+// The case setup is here:
+/* A product offer can be applied only if a person buys more than 2 items, and the offer has not expired.
+Premium members do not need to buy a specific amount of products. */
 
-let premium = prompt("Are you a premium member?");
-(function premiumCk() {
-    if (premium.toLowerCase() === "yes")
-    {
-        return true;
+// Date check to determine if offer is expired using pre-determined exp date and today's date.
+let today = new Date().toLocaleDateString()
+console.log(today)
+let offerExp = new Date(2022, 3, 40, 0, 0, 0, 0).toLocaleDateString();
+console.log(offerExp);
+
+// Need this var during final for date check.
+let expCk = (offerExp >= today);
+console.log(expCk);
+
+//function to check if person is premium member or not.
+function premiumCk() {
+    let premium = prompt("Are you a premium member?");
+    return premium.toLowerCase() === "yes";
+}
+// Need this var during final check for premium
+let isPremium = premiumCk();
+console.log(isPremium);
+
+// Check to see if two or more are purchased. Used in conjunction with premium status to determine offer validity.
+function quantityCk() {
+    let qty = prompt("How many Commodore 64s are you purchasing today?");
+    return qty >= 2;
+}
+// Need this var during final check for quantity purchased
+let qtyMoreThanTwo = quantityCk();
+console.log(qtyMoreThanTwo);
+
+// Final processing of the supplied data to determine customer's offer eligibility. Set to auto-call the function.
+(function finalCk() {
+    if (isPremium && expCk === true) {
+        alert("Congratulations, you qualify for this offer!");
+    } else if (!isPremium && expCk && qtyMoreThanTwo === true) {
+        alert("Congratulations, you qualify for this offer!");
     } else {
-        return false;
+        alert("Sorry, you do not qualify for this offer");
     }
 })()
-
-premiumCk()
 
 
