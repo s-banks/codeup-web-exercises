33e57a037a8734eaea315fbd4e24cd02c4feaedf
Shane Banks <shane.banks02@gmail.com>
2022-04-22 09:23:23 -0500

    Fizzbuzz
    git push
    exit

--- js/warmups.js
+++ js/warmups.js
@@ -1,3 +1,18 @@
 "use strict";
 
 console.log("Yay! I completed the warm-up!");
+
+function fizzBuzz() {
+	for (let i = 1; i <= 100; i++) {
+		if(i % 15 === 0) {
+			console.log("Fizzbuzz");
+		} else if (i % 5 === 0) {
+			console.log("Buzz");
+		} else if (i % 3 === 0) {
+			console.log("Fizz");
+		} else {
+			console.log(i);
+		}
+	}
+}
+fizzBuzz()
