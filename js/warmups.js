c8de9d12142094730d0f43fc671d3fd460bb0122
Shane Banks <shane.banks02@gmail.com>
2022-04-27 09:43:46 -0500

    element and image tweaks

--- js/warmups.js
+++ js/warmups.js
@@ -2,18 +2,24 @@
 
 console.log("Yay! I completed the warm-up!");
 console.log("--------------- \n---------------");
-function fizzBuzz() {
-	for (let i = 1; i <= 100; i++) {
-		if(i % 15 === 0) {
-			console.log("Fizzbuzz");
-		} else if (i % 5 === 0) {
-			console.log("Buzz");
-		} else if (i % 3 === 0) {
-			console.log("Fizz");
-		} else {
-			console.log(i);
-		}
-	}
+// function fizzBuzz() {
+// 	for (let i = 1; i <= 100; i++) {
+// 		if(i % 15 === 0) {
+// 			console.log("Fizzbuzz");
+// 		} else if (i % 5 === 0) {
+// 			console.log("Buzz");
+// 		} else if (i % 3 === 0) {
+// 			console.log("Fizz");
+// 		} else {
+// 			console.log(i);
+// 		}
+// 	}
+// }
+// fizzBuzz()
+// console.log("--------------- \n---------------");
+
+let allFirstLetters = ['dog', 'cat', 'frog', 'bat'];
+
+function stipLetters(arr) {
+
 }
-fizzBuzz()
-console.log("--------------- \n---------------");
