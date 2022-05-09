e1c75878404a37f599291e1fcbeceef137f7f936
Shane Banks <shane.banks02@gmail.com>
2022-05-09 09:25:43 -0500

    warmup complete

--- js/warmups.js
+++ js/warmups.js
@@ -64,3 +64,43 @@ console.log("--------------- \n---------------");
 //  return val.name;
 // }
 // console.log(returnProductEssentialDetails(product1));
+
+// begin this warmup
+// const hamsters = [
+// 	{
+// 		name: "Hamtaro",
+// 		heightInMM: 86,
+// 		fur: ['orange', 'white'],
+// 		gender: "male",
+// 		dateOfBirth: "August 6"
+// 	} , {
+// 		name: "Bijou",
+// 		heightInMM: 75,
+// 		fur: ['white'],
+// 		gender: "female",
+// 		dateOfBirth: "July 10"
+// 	} , {
+// 		name: "Oxnard",
+// 		heightInMM: 100,
+// 		fur: ['grey', 'white'],
+// 		gender: "male",
+// 		dateOfBirth: "May 3"
+// 	} , {
+// 		name: "Boss",
+// 		heightInMM: 120,
+// 		fur: ['brown', 'white'],
+// 		gender: "male",
+// 		dateOfBirth: "Spetember 21"
+// 	} , {
+// 		name: "Snoozer",
+// 		heightInMM: 85,
+// 		fur: ['brown', 'white', "pink"],
+// 		gender: "male",
+// 		dateOfBirth: "January 14"
+// 	}
+// ];
+//
+// const largest = hamsters.reduce((prev, current) => (+prev.heightInMM > +current.heightInMM) ? prev : current);
+// console.log(largest);
+// end of previous warmup
+
