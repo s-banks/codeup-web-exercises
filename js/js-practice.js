d69cbc63ad98596f1b3a59a7d0b60dfec25fef43
Shane Banks <shane.banks02@gmail.com>
2022-05-12 23:11:34 -0500

    finished practice

--- js/js-practice.js
+++ js/js-practice.js
@@ -129,3 +129,20 @@ console.log("--------------- \n---------------");
 
 //Next items from: https://java.codeup.com/extra-exercises/javascript/arrays/
 
+//Define a function named allIndexesOf that takes in two arguments. The first argument should be the array to search
+// and the second argument should be the value you want to search for. If the item does not exist in the provided
+// array, return an empty array.
+
+let fruits = ["apple", "banana", "orange", "apple", "pineapple"];
+function allIndexesOf(arr, val) {
+	let indexes = [];
+	for(let i = 0; i < arr.length; i++)
+		if (arr[i] === val)
+			indexes.push(i);
+	console.log(indexes);
+}
+allIndexesOf(fruits, "apple") //should return the array [0, 3]
+allIndexesOf(fruits, "guava") //should return the array []
+allIndexesOf(fruits, "pineapple") //should return [4]
+console.log("--------------- \n---------------");
+
