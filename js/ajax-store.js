e9613994fb1563928c26bdab4e773161994d1bc3
Shane Banks <shane.banks02@gmail.com>
2022-05-25 10:41:00 -0500

    removed unused testing code

--- js/ajax-store.js
+++ js/ajax-store.js
@@ -1,33 +1,5 @@
 "use strict";
 (function () {
-	// (function() {
-	// 	"use strict";
-	// 	// TODO: Create an AJAX GET request for the file under data/inventory.json
-	// 	$.get('data/inventory.json').done(function(data) {
-	// 		console.log(data);
-	// 		function appendData(data) {
-	// 			let table = document.getElementById("insertProducts");
-	// 			console.log(data);
-	// 			for (let i = 0; i < data.length; i++) {
-	// 				table.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
-	// 			}
-	// 		}
-	// 	})
-	// })();
-
-	// $.get("/data/inventory.json").done(function (tools) {
-	// 	$("#books").html("");
-	// 	tools.forEach(function(tool) {
-	// 		let trO = '<tr>';
-	// 		let trC = '</tr>';
-	// 		let td1 = "<h1>" + tool.title + "</h1>";
-	// 		let td2 = "<p>By: " + tool.author + "</p>"
-	// 		let td3 = "<h1>" + tool.title + "</h1>";
-	// 		let td4 = "<h1>" + tool.title + "</h1>";
-	// 		$("#books").append(h1 + p);
-	// 	});
-	// });
-
 
 	$.get("/data/inventory.json").done(function (obj) {
 		let tbody = document.getElementById('insertProducts');
