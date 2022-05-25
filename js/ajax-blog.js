f869b56ae8043974b4288e676d4a340aa6202d25
Shane Banks <shane.banks02@gmail.com>
2022-05-25 11:30:05 -0500

    css files in place

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -1,5 +1,12 @@
 "use strict";
 (function () {
-
+	$.get("/data/library.json").done(function (books) {
+		$("#books").html("");
+		books.forEach(function(book) {
+			var h1 = "<h1>" + book.title + "</h1>";
+			var p = "<p>By: " + book.author + "</p>"
+			$("#books").append(h1 + p);
+		});
+	});
 
 }())
