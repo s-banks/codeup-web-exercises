b5dae00a2332285e7645c6e8774a4e8a70f2bde9
s-banks <shroud2@hotmail.com>
2022-05-30 22:30:25 -0500

    more tweaks

--- js/testing.js
+++ js/testing.js
@@ -10,14 +10,14 @@
 // document.querySelector("#demo3").innerHTML = city;
 // document.querySelector("#demo4").innerHTML = locationName;
 
-
-geocode('kuwait city', MAPBOX_API_KEY).then(function(results) {
-    console.log(results);
-})
-
-reverseGeocode({lat: 29.375, lng: 47.98}, MAPBOX_API_KEY).then(function(results) {
-    console.log(results);
-})
+//
+// geocode('kuwait city', MAPBOX_API_KEY).then(function(results) {
+//     console.log(results);
+// })
+//
+// reverseGeocode({lat: 29.375, lng: 47.98}, MAPBOX_API_KEY).then(function(results) {
+//     console.log(results);
+// })
 
 
 // geocode(str, MAPBOX_API_KEY).then(function(results) {
@@ -32,3 +32,17 @@ reverseGeocode({lat: 29.375, lng: 47.98}, MAPBOX_API_KEY).then(function(results)
 //     .then(data => {
 //         console.log(data)
 // })
+
+
+//toggles the highlight class on each li
+let liList = document.querySelectorAll("li");
+for (let x of liList) {
+    x.classList.toggle("highlight");
+}
+
+
+//Set different font color based on array for all spans in a document
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+// document.querySelectorAll('span').forEach((sp, ind) => {
+//     sp.style.color = colors[ind];
+// });
