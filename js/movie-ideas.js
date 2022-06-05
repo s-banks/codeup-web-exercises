04824d4b5c4161ae2b6ded430ab9252c00e21f29
s-banks <shroud2@hotmail.com>
2022-06-05 18:06:13 -0500

    a little prethought done on movie exercise

---
+++ js/movie-ideas.js
@@ -0,0 +1,17 @@
+"use strict";
+
+(function () {
+
+
+
+    $(window).on("load", function () {
+        $(".demo-container").fadeOut(6000);
+    });
+
+
+
+
+
+
+
+}())
---
+++ js/promises.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//(function () {
+
+
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+
+
+const githubPromise = fetch('https://api.github.com/repositories');
+const bitbucketPromise = fetch('https://api.bitbucket.org/2.0/repositories');
+
+Promise.all([githubPromise, bitbucketPromise])
+    .then(function(data) {
+        // here data is an array of the resolved values from each promise
+        // we can now do something with both pieces of data
+    })
+    .catch(function(error) {
+        // handle errors
+    });
+
+//}())
